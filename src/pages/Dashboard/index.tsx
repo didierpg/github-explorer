7a060180a60b1f88f1cf4ed38c899de33aa36c8a
Didier <didierpg@gmail.com>
2020-05-31 18:54:07 -0300

    Primeiro projeto com React ▸ Criando a aplicação ▸  Estilizando Dashboard
    yarn add polished
    yarn add react-icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,92 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Resositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Resositories>
+      <a href="teste">
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png"
+          alt="Fulano"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      {/* REAPEATED */}
+      <a href="teste">
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png"
+          alt="Fulano"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png"
+          alt="Fulano"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png"
+          alt="Fulano"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png"
+          alt="Fulano"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png"
+          alt="Fulano"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      {/*  */}
+    </Resositories>
+  </>
+);
 export default Dashboard;
